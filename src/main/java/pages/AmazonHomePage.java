959fe4447915ba3490454a6e71c414c75684468b
Suranga Wijayarathna <suranga1201@gmail.com>
2023-01-23 15:45:25 +0530

    Property implementation + page classes + base test +testNG

--- src/main/java/pages/AmazonHomePage.java
+++ src/main/java/pages/AmazonHomePage.java
@@ -1,6 +1,37 @@
 package pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+
 public class AmazonHomePage {
 
-//    private
+    WebDriver driver;
+
+     By category_dropdown = By.id("searchDropdownBox");
+     By search_input = By.id("twotabsearchtextbox");
+
+    public AmazonHomePage(WebDriver driver){
+       this.driver = driver;
+   }
+    public void Select_Categorylist(){
+
+       WebElement ele = driver.findElement(category_dropdown);
+       Select drop = new Select(ele);
+       drop.selectByVisibleText("Books");
+
+}
+
+    public void Select_Searchinput(String searchtext){
+        driver.findElement(search_input).sendKeys(searchtext);
+
+
+    }
+
+    public void PressEnter(){
+        driver.findElement(search_input).sendKeys(Keys.ENTER);
+
+    }
+
+
+
 }
--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -4,13 +4,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import pages.AmazonHomePage;
 
 
-public class Test {
 
-   private WebDriver driver;
+public class BaseTest {
+
+   public static WebDriver driver;
   public static String browserName=null;
     public static String url=null;
 
@@ -21,6 +24,8 @@ public class Test {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
             driver.get(url);
+            driver.manage().window().maximize();
+
 
 
         } else if (browserName.equalsIgnoreCase("firefox")) {
@@ -28,20 +33,15 @@ public class Test {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
             driver.get(url);
-
         }
     }
 
-    @org.testng.annotations.Test
-    public static void test(){
-        System.out.println("hi");
-    }
 
     @AfterTest
     public void tearDown(){
-
         driver.quit();
         System.out.println("Test is completed");
+
 }
 
 }
